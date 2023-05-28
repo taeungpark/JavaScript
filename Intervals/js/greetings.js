0ae125292bbb4dd5a9112e9b640a6f64a1233c70
Taeung <park0579@algonquinlive.com>
2023-05-28 14:24:35 -0400

    Intervals added

---
+++ Intervals/js/greetings.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDENT_CLASSNAME = "hidden";
+const USERNAME_KEY ="username"
+
+function onLoginSubmit(event){
+
+    event.preventDefault();
+    loginForm.classList.add("hidden");
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
+}
+
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDENT_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDENT_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    paintGreetings()
+}
